6e2844f838dff88a33f2c0ee56ea5ab0a34387e3
luucamay <lupemaydana@gmail.com>
2020-01-23 18:15:47 -0400

    trying to make a first request petition

--- src/Login.js
+++ src/Login.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios'
 class TextField extends React.Component {
     constructor(props) {
         super(props);
@@ -18,7 +19,7 @@ class TextField extends React.Component {
 class Button extends React.Component {
     constructor(props) {
         super(props);
-        this.state ={
+        this.state = {
         }
     }
     render() {
@@ -36,6 +37,34 @@ class Login extends React.Component {
             username: '',
             password: ''
         }
+        this.handleClick = this.handleClick.bind(this)
+    }
+    handleClick(event) {
+        var apiBaseUrl = "http://localhost:4000/api/";
+        var self = this;
+        var payload = {
+            "email": this.state.username,
+            "password": this.state.password
+        }
+        axios.post(apiBaseUrl + 'login', payload)
+            .then(function (response) {
+                console.log(response);
+                if (response.data.code == 200) {
+                    console.log("Login successfull");
+                    
+                }
+                else if (response.data.code == 204) {
+                    console.log("Username password do not match");
+                    alert("username password do not match")
+                }
+                else {
+                    console.log("Username does not exists");
+                    alert("Username does not exist");
+                }
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
     }
     render() {
         return (

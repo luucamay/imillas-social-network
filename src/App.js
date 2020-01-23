7b7178d740d206195b5f12c6e4e3db86fac55f03
luucamay <lupemaydana@gmail.com>
2020-01-23 16:43:59 -0400

    Creating the first view

--- src/App.js
+++ src/App.js
@@ -6,18 +6,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Login</h1>
+        <h2>Email:</h2>
+        <input></input>
+        <h2>Password:</h2>
+        <input></input>
+        <hr /> {/*remove this and customize css of the button*/}
+        <button>Login</button>
       </header>
     </div>
   );

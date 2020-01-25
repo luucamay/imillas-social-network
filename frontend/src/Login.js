3c4f639ad957bdced7d2e0569c85abe9b2e1a251
luucamay <lupemaydana@gmail.com>
2020-01-25 05:56:20 -0400

    Adding the request for the login with axios

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,63 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
-class Login extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            user: null
-        }
-        this.handleClick = this.handleClick.bind(this)
-        this.handleClick = this.handleSignIn.bind(this)
+const Login = () => {
+    const [user, setUser] = useState({
+        email: '',
+        password: ''
+    });
+
+    const setEmail = (newValue) => {
+        setUser({
+            email: newValue,
+            ...user
+        })
     }
-    handleSignIn(event) {
-        event.preventDefault()
-        let username = this.state.username.value
-        let password = this.state.password.value
-        this.props.onSignIn(username, password)
+
+    const setPassword = (newValue) => {
+        setUser({
+            password: newValue,
+            ...user
+        })
     }
-    handleClick(event) {
-        let apiBaseUrl = "http://localhost:8080/users/";
-        // carga util, osea datos que vamos a enviar
-        let payload = {
-            "email": this.state.username,
-            "password": this.state.password
-        }
-        //axios.post(apiBaseUrl + 'login', payload)
-        axios.get(apiBaseUrl)
+
+    const login = () => {
+        let apiBaseUrl = "http://localhost:8080";
+        axios.post(apiBaseUrl + '/login', user)
             .then(function (response) {
                 console.log(response);
-                if (response.data.code === 200) {
+                if (response.status === 200) {
                     console.log("Login successfull");
-                    this.setState({
-                        logged: true
-                    });
+                    localStorage.setItem('_TOKEN', response.data.token)
+                    localStorage.setItem('username', response.data.username)
                     // redirect to social media page! Or load social media page!!!
                 }
-                else if (response.data.code === 204) {
-                    console.log("Username password do not match");
-                    alert("username password do not match") // change this
+                else if (response.status === 204) {
+                    console.log("El usuario y la contrasenia no coinciden");
+                    alert("El usuario y la contrasenia no coinciden") // change this
                 }
                 else {
-                    console.log("Username does not exists");
-                    alert("Username does not exist"); // change this
+                    console.log("El usuario no existe");
+                    alert("El usuario no existe"); // change this
                 }
             })
             .catch(function (error) {
                 console.log(error);
             });
     }
-    render() {
-        return (
-            <form onSubmit={this.handleClick}>
-                <h1>Login</h1>
-                <input type="text" username="username" placeholder="enter you username" /><br />
-                <input type="password" password="password" placeholder="enter password" /><br />
-                
-                <input type="submit" value="Login" />
-            </form>
-        );
-    }
+    return (
+        <form onSubmit={login}>
+            <h1>Login</h1>
+            <input type="text" username="username" placeholder="enter you username" /><br />
+            <input type="password" password="password" placeholder="enter password" /><br />
+
+            <input type="submit" value="Login" />
+        </form>
+    );
 }
 
 export default Login;

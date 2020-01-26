69c7edad7faa836967403b82eb2ae137b7081006
luucamay <lupemaydana@gmail.com>
2020-01-26 17:32:28 -0400

    created the model for a post

---
+++ backend/models/post.js
@@ -0,0 +1,49 @@
+const { dbPosts } = require('./db');
+
+// Insert a new post in the database and return its id.
+exports.create = function (user_id, content, type, callback) {
+    const post = {
+        user_id: user_id,
+        content: content,
+        type: type
+    }
+    dbPosts.insert(post, callback)
+}
+
+// Get all posts.
+exports.all = function (callback) {
+    dbPosts.find({}).sort({
+        updatedAt: -1
+    }).exec(callback)
+}
+
+// Get all posts from specific type.
+exports.allOfType = function (post_type, callback) {
+    dbPosts.find({
+        type: post_type
+    }).sort({
+        updatedAt: -1
+    }).exec(callback)
+}
+
+// Get a post by id.
+exports.getOne = function (post_id, callback) {
+    const post = {
+        _id: post_id
+    }
+    dbPosts.findOne(post).exec(callback)
+}
+
+// Update a post.
+exports.update = function(post, callback){
+    dbPosts.update({
+        _id: post.post_id
+    }, post, {}, callback)
+}
+
+// Delete the post.
+exports.delete = function (post_id, callback) {
+    dbPosts.remove({
+        _id: post_id
+    }, {}, callback)
+}
